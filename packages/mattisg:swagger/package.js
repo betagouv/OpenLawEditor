9e94596af8b7395c2ef3553359d541df4ca404f6
Matti Schneider <matti.schneider@data.gouv.fr>
2014-12-17 10:38:16 +0100

    Refactor mattisg:swagger package file hierarchy
    Export all necessary files

--- packages/mattisg:swagger/package.js
+++ packages/mattisg:swagger/package.js
@@ -5,11 +5,27 @@ Package.describe({
 	git: 'git://github.com/MattiSG/meteor-swagger-ui'
 });
 
+/** Creates an array-creating function, facilitating the addition of many files in the same directory.
+*
+*@param	{String}	directory	Prefix for the paths. The trailing slash will be inserted for you, don't add it.
+*@param	{String}	extension	Suffix for the paths. The leading dot will be inserted for you, don't add it.
+*@returns	{<Array<String>>Function<String>}	A function taking a space-separated string listing all files within the given directory and with the given extension.
+*/
+function all(directory, extension) {
+	return function(filenames) {
+		return filenames.split(' ').map(function(filename) {
+			return directory + '/' + filename + '.' + extension;
+		});
+	}
+}
+
 Package.onUse(function(api) {
 	api.versionsFrom('1.0.1');
 	api.use([ 'templating' ], 'client');
 	api.use('tap:i18n@1.2.1');
-	api.addFiles([ 'mattisg:swagger.js', 'swagger.html' ]);
+	api.addFiles('mattisg:swagger.js');
+	api.addFiles(all('templates', 'html')('parameter path response swagger'));
+	api.addFiles(all('i18n', 'fr.i18n.json')('helpers swagger path'));
 });
 
 Package.onTest(function(api) {
