3644c2e2769bda9c4ca8ae5e9cdcd7318dfdd733
Matti Schneider <matti.schneider@data.gouv.fr>
2014-12-18 10:00:19 +0100

    Fetch Swagger resource declaration in package

--- packages/mattisg:swagger/client/templates/swagger.js
+++ packages/mattisg:swagger/client/templates/swagger.js
@@ -1,13 +1,36 @@
 Template.swagger.created = function() {
-	try {
-		check(this.data);
-	} catch(err) {
-		console.error(err, 'We will still render. Please report any layout issues you get.');
-	}
+	HTTP.get(this.data.resourceDeclaration, function(error, result) {
+		if (error)	// TODO: handle errors
+			throw error;
+
+		Session.set('resourceDeclaration', result.data);
+	});
+
+	this.autorun(function() {
+		var resourceDeclaration = Session.get('resourceDeclaration');
+
+		if (! resourceDeclaration)
+			return;
+
+		try {
+			check(resourceDeclaration);
+		} catch(err) {
+			console.error(err, 'We will still render. Please report any layout issues you get.');
+		}
+	});
 }
 
+
 Template.swagger.helpers({
+	resourceDeclaration: function() {
+		return Session.get('resourceDeclaration');
+	},
+	info: function() {
+		if (Session.get('resourceDeclaration'))
+			return Session.get('resourceDeclaration').info;
+	},
 	paths: function() {
-		return makePathsIterable(Template.instance().data.paths);
+		if (Session.get('resourceDeclaration'))
+			return makePathsIterable(Session.get('resourceDeclaration').paths);
 	}
 });
