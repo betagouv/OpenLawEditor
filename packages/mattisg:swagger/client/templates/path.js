99c153e4623aec4d9f6e7df229fb6f3b3c923fe4
Matti Schneider <matti.schneider@data.gouv.fr>
2014-12-19 20:42:55 +0100

    Make the swagger operation doc a form

--- packages/mattisg:swagger/client/templates/path.js
+++ packages/mattisg:swagger/client/templates/path.js
@@ -1,4 +1,15 @@
 Template.swagger_path.helpers({
+	pathPrefix: function() {
+		var resourceDeclaration = Session.get('resourceDeclaration');
+
+		if (resourceDeclaration) {
+			var basePath	= resourceDeclaration.basePath,
+				host		= resourceDeclaration.host,
+				scheme		= resourceDeclaration.schemes[0];
+
+			return scheme + '://' + host + basePath;
+		}
+	},
 	responses: function() {
 		return makeResponsesIterable(Template.instance().data.responses);
 	}
