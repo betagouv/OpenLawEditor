536390575e6392fc19d4ad71ed820f8407f7f2fb
Matti Schneider <matti.schneider@data.gouv.fr>
2014-12-29 16:21:48 +0100

    Add parameters to Swagger action

--- packages/mattisg:swagger/client/templates/path.js
+++ packages/mattisg:swagger/client/templates/path.js
@@ -2,23 +2,35 @@ Template.swagger_path.created = function() {
 	this.data.parameterValues = new ReactiveDict();	// store in .data to provide access to subtemplates through `parentData`
 }
 
-
-Template.swagger_path.helpers({
-	pathPrefix: function() {
-		var	DEFAULT_PORT = 80,
-			resourceDeclaration = Session.get('resourceDeclaration'),
-			uriParser = document.createElement('a');
+function pathPrefix() {
+	var	DEFAULT_PORT = 80,
+		resourceDeclaration = Session.get('resourceDeclaration'),
+		uriParser = document.createElement('a');
 
 		uriParser.href = Session.get('resourceDeclarationUrl');
 
-		if (resourceDeclaration) {
-			var basePath	= resourceDeclaration.basePath,
-				protocol	=	resourceDeclaration.schemes[0] + ':' || uriParser.protocol,
-				host		=	resourceDeclaration.host
-								|| uriParser.hostname + (uriParser.port != DEFAULT_PORT ? ':' + uriParser.port : '');
+		var basePath	=	resourceDeclaration && resourceDeclaration.basePath,
+			protocol	=	(resourceDeclaration && resourceDeclaration.schemes ? resourceDeclaration.schemes[0] + ':' : uriParser.protocol),
+			host		=	resourceDeclaration && resourceDeclaration.host
+							|| uriParser.hostname + (uriParser.port != DEFAULT_PORT ? ':' + uriParser.port : '');
 
-			return protocol + '//' + host + basePath;
-		}
+		return protocol + '//' + host + basePath;
+}
+
+
+Template.swagger_path.helpers({
+	url: function() {
+		var parameters = [];
+
+		Template.instance().data.parameters.forEach(function(description) {
+			var name =	description.name,
+				value =	Template.instance().data.parameterValues.get(name);
+
+			if (value)
+				parameters.push(name + '=' + value);
+		});
+
+		return pathPrefix() + Template.instance().data.path + (parameters.length ? '?' : '') + parameters.join('&');
 	},
 
 	responses: function() {
